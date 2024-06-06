e826b4ad10091017f234222227ccc49842870a02
Devops Lab <168528732+Carolinjogy@users.noreply.github.com>
2024-06-06 19:53:21 +0530

    Create Project3.java

---
+++ Project3.java
@@ -0,0 +1,9 @@
+
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class Project3 {
+  public static void main(String[] args) {
+    System.out.println("Welcome to DevOps.");
+  }
+}
